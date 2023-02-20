4d5e7a8ef0e90dc1c10100fad8edaca29e770c01
hafizhilhamm <hafizhilham36.com>
2023-02-20 21:43:56 +0700

    realsense

---
+++ devel/share/realsense2_camera/cmake/realsense2_camera-msg-extras.cmake
@@ -0,0 +1,2 @@
+set(realsense2_camera_MESSAGE_FILES "/home/hafizh/catkin_ws/src/realsense-ros/realsense2_camera/msg/IMUInfo.msg;/home/hafizh/catkin_ws/src/realsense-ros/realsense2_camera/msg/Extrinsics.msg;/home/hafizh/catkin_ws/src/realsense-ros/realsense2_camera/msg/Metadata.msg")
+set(realsense2_camera_SERVICE_FILES "/home/hafizh/catkin_ws/src/realsense-ros/realsense2_camera/srv/DeviceInfo.srv")
